5d74cc55e3424816095a01bd99157c311dcefc63
ardulogic <denncath@gmail.com>
2019-04-25 13:27:43 +0300

    v1.3

--- core/classes/user/Repository.php
+++ core/classes/user/Repository.php
@@ -2,7 +2,13 @@
 
 namespace Core\User;
 
-class Repository extends \Core\User\Model {
+class Repository {
+
+    protected $model;
+
+    public function __construct(\Core\Database\Connection $c) {
+        $this->model = new \Core\User\Model($c);
+    }
 
     public function register(\Core\User\User $user) {
         if (!$this->exists($user)) {
@@ -13,9 +19,48 @@ class Repository extends \Core\User\Model {
 
         return self::REGISTER_ERR_EXISTS;
     }
+
+    public function insert(\Core\User\User $user) {
+        return $this->model->insertIfNotExists(
+                $user->getData(), ['email']
+        );
+    }
     
-    public function insert($user) {        
-        return parent::insert($user->getData());
+    public function load($email) {
+       $rows = $this->model->load([
+           'email' => $email 
+        ]);
+       
+        foreach ($rows as $row) {
+            return new \Core\User\User($row);  
+        }
     }
 
+    
+    public function loadAll() {
+       $rows = $this->model->load();
+       $users = [];
+       
+        foreach ($rows as $row) {
+            $users[] = new \Core\User\User($row);  
+        }
+        
+        return $users;
+    }
+    
+    public function update(\Core\User\User $user) {
+        return $this->model->update($user->getData(), [
+            'email' => $user->getEmail()
+        ]);
+    }
+    
+    public function delete(\Core\User\User $user) {
+        return $this->model->delete([
+            'email' => $user->getEmail()
+        ]);
+    }
+    
+    public function deleteAll() {
+        return $this->model->delete();
+    }
 }
--- public_html/index.php
+++ public_html/index.php
@@ -2,26 +2,25 @@
 require_once '../bootloader.php';
 
 $connection = new \Core\Database\Connection(DB_CREDENTIALS);
-
-$pdo = $connection->getPDO();
 $schema = new \Core\Database\Schema($connection, DB_NAME);
-$schema->init();
 
 $repository = new \Core\User\Repository($connection);
-$user = new \Core\User\User(['email' => 'valdau@' . rand(0, 100) . 'nx.com',
+
+//READY
+
+$user = new \Core\User\User([
+    'email' => 'valdau@nxxsda.comm',
     'password' => 'bbd',
     'full_name' => 'Zanas Vandamas',
     'account_type' => \Core\User\User::ACCOUNT_TYPE_USER,
     'is_active' => true,
     'age' => 26,
     'gender' => 'm'
-        ], [
-    'email'
-]);
+        ]);
 
-$repository->insert($user);
-$users = $repository->load();
-
+$success = $repository->insert($user);
+var_dump('success', $success);
+$users = $repository->loadAll();
 ?>
 <html>
     <head>
@@ -30,12 +29,10 @@ $users = $repository->load();
     <body>
         <?php foreach ($users as $user): ?>
             <ul>
-                <?php foreach ($user as $col => $value): ?>
-                    <li>
-                        <b><?php print $col; ?></b>
-                        <i><?php print $value; ?></i>
-                    </li>
-                <?php endforeach; ?>
+                <li>
+                    <b>Email:</b>
+                    <i><?php print $user->getEmail(); ?></i>
+                </li>
             </ul>
         <?php endforeach; ?>
     </body>
